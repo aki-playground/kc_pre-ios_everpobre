095fe5f593448161f847a28b762a0e9920afa57c
Akixe <akixe.otegi@gmail.com>
2016-02-29 06:41:26 +0100

    Momentos adecuados para guardar

--- Everpobre/AppDelegate.m
+++ Everpobre/AppDelegate.m
@@ -35,11 +35,16 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    [self save];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    //Es buen momento para guardar datos
+    [self save];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -60,7 +65,21 @@
 - (void) trastearConDatos{
     AOANotebook *novias = [AOANotebook notebookWithName:@"Ex-novias" context:self.model.context];
     
-    AOANote *camila = [AOANote noteWithName:@"Camila" notebook:novias context:self.model.context];
-    AOANote *pampita = [AOANote noteWithName:@"Pampita" notebook:novias context:self.model.context];
+    [AOANote noteWithName: @"Camila"
+                 notebook: novias
+                  context: self.model.context];
+    [AOANote noteWithName: @"Pampita"
+                 notebook: novias
+                  context: self.model.context];
+    
+    //Búsquedas
+    
+    [self save];
+}
+
+-(void) save {
+    self.model saveWithErrorBlock:^(NSError *error) {
+        NSLog(@"Error al guardar %s \n\n %@", __func__, error); //__func__ lo entiende el compilador y te da el nombre de la función
+    }
 }
 @end
