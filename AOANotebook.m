614d93e759a5bcb6d9d117780be254334619351e
Akixe <akixe.otegi@gmail.com>
2016-02-29 05:42:23 +0100

    KVO en AOANotebook y ciclo de vida de un NSManagedObject

--- AOANotebook.m
+++ AOANotebook.m
@@ -2,13 +2,16 @@
 
 @interface AOANotebook ()
 
-// Private interface goes here.
++(NSArray *) observableKeyNames;
+
 
 @end
 
 @implementation AOANotebook
 
-// Custom logic goes here.
++(NSArray *) observableKeyNames{
+    return @[@"creationDate", @"name", @"notes"];;
+}
 
 +(instancetype) notebookWithName:(NSString *) name context: (NSManagedObjectContext *) context{
     AOANotebook *nb = [NSEntityDescription insertNewObjectForEntityForName:[AOANotebook entityName] inManagedObjectContext:context];
@@ -19,4 +22,51 @@
     return nb;
 }
 
+#pragma mark  - KVO
+
+- (void) setupKVO {
+   
+    for (NSString *key in [AOANotebook observableKeyNames]) {
+        [self addObserver: self
+               forKeyPath: key
+                  options: NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld
+                  context: NULL];
+    }
+}
+
+-(void) tearDownKVO {
+    for (NSString *key in [AOANotebook observableKeyNames]) {
+        [self removeObserver:self
+                  forKeyPath:key];
+    }
+}
+
+- (void) observeValueForKeyPath: (NSString *) keyPath
+                       ofObject: (id)object
+                         change: (NSDictionary *) change
+                        context: (void *) context{
+    
+    self.modificationDate = [NSDate date];
+    
+}
+
+#pragma mark - Lifecycle
+//Poner los observadores activos o mandarlos a T.P.C.
+-(void) awakeFromInsert {
+    //Siempre que se crea el objeto
+    [super awakeFromInsert];
+    [self setupKVO];
+}
+
+-(void) awakeFromFetch {
+    //Siempre que se crea el objeto o vuelve de Fault
+    [super awakeFromFetch];
+    [self setupKVO];
+}
+
+-(void) willTurnIntoFault {
+    //Se va a Fault
+    [super willTurnIntoFault];
+    [self tearDownKVO];
+}
 @end
